509a469312e9dd28d5c43d6fb601dcdb2e384ae6
ChathuraHerath <chathuradeepana7@gmail.com>
2020-08-11 18:46:46 +0530

    before major testing

--- backend/Controllers/semester/one_timeConfig-controller.js
+++ backend/Controllers/semester/one_timeConfig-controller.js
@@ -9,7 +9,8 @@ function one_timeConfigController(req,res) {
         // newConfig.email = req.body.email
         // newConfig.password = req.body.password
         // array ={A_plus: 1, A: 25}
-        
+        console.log("dplus",req.body[0].Ctrl_D_plus);
+        newConfig.userID = req.body[2].UserID
         newConfig.A_plus = req.body[0].Ctrl_A_plus
         newConfig.A = req.body[0].Ctrl_A
         newConfig.A_minus = req.body[0].Ctrl_A_minus
@@ -19,7 +20,7 @@ function one_timeConfigController(req,res) {
         newConfig.C_plus = req.body[0].Ctrl_C_plus
         newConfig.C = req.body[0].Ctrl_C
         newConfig.C_minus = req.body[0].Ctrl_C_minus
-        newConfig.D_plus = req.body[0].Ctrl_D_plus
+        newConfig.D_plus = req.body[0].Ctrl_D_plus ? req.body[0].Ctrl_D_plus : 0
         newConfig.D = req.body[0].Ctrl_D
         newConfig.I = req.body[0].Ctrl_I
         newConfig.F = req.body[0].Ctrl_F
@@ -31,19 +32,55 @@ function one_timeConfigController(req,res) {
         newConfig.class_SL_max = req.body[1].Ctrl_SecondLowerMax
         newConfig.class_pass_min = req.body[1].Ctrl_PassMin
         newConfig.class_pass_max = req.body[1].Ctrl_PassMax
+
+        console.log("newConfig type",typeof(newConfig._id),newConfig._id, "newConfig",newConfig);
         
-        
+        one_timeConfig.findOne({userID:req.body[2].UserID}).then(
+                
+               x=>{
+                console.log("one time", !!x)
+                       if(!x){
+                        newConfig.save().then(data =>{
+                                console.log("data =>",data)
+                        return res.json({msg: "OK"});
+                
+                        }).catch(err => {
+                                return res.status(404).send({errormsg: 'one time config failed'+ err})
+                        })  
+                       }
+                       else{
+                               console.log("findone xxx",typeof(JSON.stringify(x._id)));
+                        newConfig._id = x._id;
+                        console.log("newConfig 2 type",typeof(newConfig._id),newConfig._id, "newConfig", newConfig);
+                        // newConfig.save().then(data =>{
+                        //         console.log("data =>",data)
+                        // return res.json({msg: "OK"});
+                
+                        // }).catch(err => {
+                        //         return res.status(404).send({errormsg: 'one time config failed'+ err})
+                        // })
+                        newConfig.save().then(data =>{
+                                console.log("data =>",data)
+                        return res.json({msg: "OK"});
+                
+                        }).catch(err => {
+                                return res.status(404).send({errormsg: 'one time config failed'+ err})
+                        })
+
+                       }
+               }
+        )
         
 
-        console.log("one time data ==>", req.body, "ff",req.body[0].Ctrl_A_plus)
+        console.log("one time data ==>", req.body)
 
-        newConfig.save().then(data =>{
-                // console.log("data =>",data)
-        return res.json({msg: "OK"});
+        // newConfig.save().then(data =>{
+        //         console.log("data =>",data)
+        // return res.json({msg: "OK"});
 
-        }).catch(err => {
-                return res.status(404).send({errormsg: 'one time config failed'+ err})
-        })
+        // }).catch(err => {
+        //         return res.status(404).send({errormsg: 'one time config failed'+ err})
+        // })
 
 
 
